9d2d1f9c65ed51f0aa0628ed96fbf7f26b1a2f7e
konradgnat <konradmgnat@gmail.com>
2020-03-11 21:00:58 -0500

    resize img

--- imageRecognitionCoreML/ViewController.swift
+++ imageRecognitionCoreML/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var pictureImageView :UIImageView!
     @IBOutlet weak var titleLabel :UILabel!
     
+    private var model :Inceptionv3 = Inceptionv3()
+    
     let images = ["cat.jpg", "dog.jpg", "banana.jpg", "lion.jpg"]
     var index = 0;
 
@@ -28,6 +30,15 @@ class ViewController: UIViewController {
         }
         let img = UIImage(named: images[index])
         self.pictureImageView.image = img
+        
+        let resizedImage = img?.resizeTo(size: CGSize(width: 299, height: 299))
+        
+        let buffer = resizedImage?.toBuffer()
+        
+        let prediction = try! self.model.prediction(image: buffer!)
+        
+        self.titleLabel.text = prediction.classLabel
+        
         index = index + 1;
     }
 
